f48a63621acff4c1506ce5abe2d68df501147931
sahaj <sahajkaur7777@gmail.com>
2024-09-29 00:46:00 +0530

    implemented the popup and text area.

--- eisen.js
+++ eisen.js
@@ -1,6 +1,20 @@
 function helloWorldOfMaxi() {
   console.log('hello world of maxi');
 }
+
 var element = document.getElementById('h12');
-element.innerHTML = 'maxi';
+element.innerHTML = 'Eisenhower Matrix';
 helloWorldOfMaxi();
+
+function showInputPopup() {
+  document.getElementById("inputPopup").style.display = "block";
+}
+
+function closePopup() {
+  document.getElementById("inputPopup").style.display = "none";
+}
+
+function addTask() {
+  var taskText = document.getElementById("taskInput").value;
+  document.getElementById("taskInput").value = "";
+}
