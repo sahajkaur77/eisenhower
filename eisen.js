c7e5c9936502d5a5866aad4481366c0d9b9533e9
sahaj <sahajkaur7777@gmail.com>
2024-09-30 22:48:10 +0530

    implemented popup effect and icon related coding

--- eisen.js
+++ eisen.js
@@ -1,20 +1,26 @@
-function helloWorldOfMaxi() {
-  console.log('hello world of maxi');
-}
+  function helloWorldOfMaxi() {
+    console.log('hello world of maxi');
+  }
 
-var element = document.getElementById('h12');
-element.innerHTML = 'Eisenhower Matrix';
-helloWorldOfMaxi();
+  var element = document.getElementById('h12');
+  element.innerHTML = 'Eisenhower Matrix';
+  helloWorldOfMaxi();
 
-function showInputPopup() {
-  document.getElementById("inputPopup").style.display = "block";
-}
+  function showInputPopup() {
+    document.getElementById("inputPopup").style.display = "block";
+  }
 
-function closePopup() {
-  document.getElementById("inputPopup").style.display = "none";
-}
+  function closePopup() {
+    document.getElementById("inputPopup").style.display = "none";
+  }
 
-function addTask() {
-  var taskText = document.getElementById("taskInput").value;
-  document.getElementById("taskInput").value = "";
-}
+  function addTask() {
+    var taskText = document.getElementById("taskInput").value;
+    document.getElementById("taskInput").value = "";
+  }
+  function closePopup() {
+    document.getElementById("inputPopup").style.animation = "popup-hide 0.3s ease-in-out forwards";
+    setTimeout(function() {
+      document.getElementById("inputPopup").style.display = "none";
+    }, 300); // Adjust the delay to match the animation duration
+  }
