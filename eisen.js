ab9a3644f351cea112e3c308418436e155b4f579
sahaj <sahajkaur7777@gmail.com>
2024-10-13 21:33:12 +0530

    added the date and time picker

--- eisen.js
+++ eisen.js
@@ -5,15 +5,12 @@
   var element = document.getElementById('h12');
   element.innerHTML = 'Eisenhower Matrix';
   helloWorldOfMaxi();
-
-  function showInputPopup() {
+ 
+function showInputPopup() {
+    document.getElementById("inputPopup").style.display = "none";
+    document.getElementById("inputPopup").style.animation = "popup-show 0.3s ease-in-out forwards";
     document.getElementById("inputPopup").style.display = "block";
   }
-
-  function closePopup() {
-    document.getElementById("inputPopup").style.display = "none";
-  }
-
   function addTask() {
     var taskText = document.getElementById("taskInput").value;
     document.getElementById("taskInput").value = "";
@@ -22,5 +19,5 @@
     document.getElementById("inputPopup").style.animation = "popup-hide 0.3s ease-in-out forwards";
     setTimeout(function() {
       document.getElementById("inputPopup").style.display = "none";
-    }, 300); // Adjust the delay to match the animation duration
+    }, 300);
   }
